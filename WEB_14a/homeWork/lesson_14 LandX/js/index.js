6b55e006f42a30dd1f1715cc9e0ae423e941ffab
sharkyBy <valik_1973@mail.ru>
2019-11-02 18:33:57 +0300

    add JS

---
+++ WEB_14a/homeWork/lesson_14 LandX/js/index.js
@@ -0,0 +1,48 @@
+console.log(document);
+console.log(document.getElementById("price2"));
+console.log(document.getElementsByClassName("video-img"));
+console.log(document.getElementsByTagName("input"));
+let a;
+a=document.getElementById("price1");
+console.log(a);
+console.log(typeof(a));
+let myString="stroka";
+console.log(typeof(myString));
+let myNumber=2345;
+console.log(typeof(myNumber));
+let myObject={};
+console.log(typeof(myObject));
+let myBoolean=true;
+console.log(typeof(myBoolean));
+let myUnderfind;
+console.log(typeof(myUnderfind));
+let testObject = {};
+console.log(typeof(testObject));
+console.log(typeof 125);
+let summa=myNumber+myNumber;
+console.log(summa);
+let newMyString=myString+" "+myString;
+console.log(newMyString);
+let newPrice=document.getElementById("price1");
+newPrice.style.color="blue";
+let topRow=document.getElementById('topRow');
+topRow.style.backgroundColor='lightgray';
+topRow.style.color='black';
+document.getElementById('price3').innerHTML="$499 ";
+let priceNew=document.getElementById('price3');
+priceNew.innerHTML='<div> new new</div> ';
+priceNew.style.color="red";
+priceNew.style.textTransform="uppercase";
+console.log(priceNew.children);
+myString=myString+myString+myNumber;
+console.log(myString);
+myString="2355";
+let example=+myString-myNumber;
+console.log(example);
+console.log(typeof(example));
+
+
+
+
+
+
